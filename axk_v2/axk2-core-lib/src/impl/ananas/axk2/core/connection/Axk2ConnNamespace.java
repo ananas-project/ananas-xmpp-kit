47aea95052d964001cd45a0fb061f5af941d6a2e
xukun <xukun@fuyoo.net>
2013-07-16 15:40:31 +0800

    rename : MyBPNamespace -> Axk2ConnNamespace

--- axk_v2/axk2-core-lib/src/impl/ananas/axk2/core/connection/Axk2ConnNamespace.java
+++ axk_v2/axk2-core-lib/src/impl/ananas/axk2/core/connection/Axk2ConnNamespace.java
@@ -2,9 +2,9 @@ package impl.ananas.axk2.core.connection;
 
 import ananas.blueprint4.core.namespace.AbstractBPNamespace;
 
-public class MyBPNamespace extends AbstractBPNamespace {
+public class Axk2ConnNamespace extends AbstractBPNamespace {
 
-	public MyBPNamespace() {
+	public Axk2ConnNamespace() {
 		super("namespace.properties");
 	}
 
