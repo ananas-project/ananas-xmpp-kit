d10f370445cf6f1e82ec787db778624f583705b1
xukun <xukun0217@gmail.com>
2013-07-21 20:40:09 +0800

    ignore : account.properties

--- axk_v2/axk2-core-lib/src/impl/ananas/axk2/core/base/Engine.java
+++ axk_v2/axk2-core-lib/src/impl/ananas/axk2/core/base/Engine.java
@@ -4,25 +4,45 @@ import ananas.axk2.core.XmppCommand;
 import ananas.axk2.core.XmppEvent;
 import ananas.axk2.core.XmppFilter;
 import ananas.axk2.core.api.IClient;
+import ananas.axk2.engine.XEngine;
+import ananas.axk2.engine.XEngineFactory;
+import ananas.axk2.engine.util.DefaultEngineContext;
 import ananas.lib.util.logging.Logger;
 
 public class Engine extends Filter implements IClient {
 
 	final static Logger log = Logger.Agent.getLogger();
+	private XEngine _engine;
 
 	@Override
 	public void connect() {
-		// TODO Auto-generated method stub
+		XEngine engine = this.__getEngine();
+		engine.connect();
+	}
+
+	private XEngine __getEngine() {
+
+		XEngine engine = this._engine;
+		if (engine != null) {
+			return engine;
+		}
 
 		this.getConnection().send(new MyTraceCmd());
 		this.getConnection().dispatch(new MyTraceEvent());
 
+		XEngineFactory factory = XEngineFactory.Agent.getDefault();
+		DefaultEngineContext context = new DefaultEngineContext();
+		context._account = this.getConnection().getAccount();
+		log.info("create xmpp client to " + context._account);
+		engine = factory.createEngine(context);
+
+		return engine;
 	}
 
 	@Override
 	public void disconnect() {
-		// TODO Auto-generated method stub
-
+		XEngine engine = this.__getEngine();
+		engine.disconnect();
 	}
 
 	class MyTraceCmd implements XmppCommand {
