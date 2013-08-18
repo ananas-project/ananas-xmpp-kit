9164e014befca7482ec8870e4586071bacc2d3a5
xukun <xukun0217@gmail.com>
2013-08-18 20:27:13 +0800

    impl: disco#info

--- axk_v2/axk2-kit1-lib/src-xep-0030/ananas/axk2/xep/xep_0030/disco/info/Tar_query.java
+++ axk_v2/axk2-kit1-lib/src-xep-0030/ananas/axk2/xep/xep_0030/disco/info/Tar_query.java
@@ -1,5 +1,19 @@
 package ananas.axk2.xep.xep_0030.disco.info;
 
+import java.util.List;
+import java.util.Vector;
+
 public class Tar_query {
+
+	private final List<Tar_identity> _list_identity = new Vector<Tar_identity>();
+	private final List<Tar_feature> _list_featue = new Vector<Tar_feature>();
+
+	public void add_feature(Tar_feature feature) {
+		this._list_featue.add(feature);
+	}
+
+	public void add_identity(Tar_identity identity) {
+		this._list_identity.add(identity);
+	}
 
 }
--- axk_v2/axk2-kit1-lib/src-xep-0030/ananas/axk2/xep/xep_0030/disco/info/bp/Ctr_query.java
+++ axk_v2/axk2-kit1-lib/src-xep-0030/ananas/axk2/xep/xep_0030/disco/info/bp/Ctr_query.java
@@ -1,5 +1,34 @@
 package ananas.axk2.xep.xep_0030.disco.info.bp;
 
+import ananas.axk2.xep.xep_0030.disco.info.Tar_feature;
+import ananas.axk2.xep.xep_0030.disco.info.Tar_identity;
+import ananas.axk2.xep.xep_0030.disco.info.Tar_query;
+import ananas.blueprint4.core.lang.BPElement;
+import ananas.blueprint4.core.lang.BPNode;
+
 public class Ctr_query extends CObject {
+
+	public Tar_query target_query() {
+		return (Tar_query) this.getTarget(true);
+	}
+
+	@Override
+	protected boolean onAppendChild(BPNode node) {
+
+		if (node instanceof BPElement) {
+			Object ch_tar = ((BPElement) node).getTarget(true);
+			if (ch_tar instanceof Tar_feature) {
+				Tar_feature feature = (Tar_feature) ch_tar;
+				this.target_query().add_feature(feature);
+				return true;
+			} else if (ch_tar instanceof Tar_identity) {
+				Tar_identity identity = (Tar_identity) ch_tar;
+				this.target_query().add_identity(identity);
+				return true;
+			}
+		}
+
+		return super.onAppendChild(node);
+	}
 
 }
